f9bc75bbb4ed76536627bf869ce0969e149e321b
Tan Le <tle@defisolutions.com>
2019-03-25 10:45:05 +0700

    Init

---
+++ MinimalProject/TC01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace MinimalProject
+{
+    [TestFixture]
+    public class TC01
+    {
+        private ChromeDriver driver;
+
+        [Test]
+        public void MyExample()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(new Uri("http://google.com"));
+            var assertion = true;
+            assertion.Should().BeTrue("Oke");
+            assertion.Should().BeFalse("Ko Oke");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
